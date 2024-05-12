e86764fca0f75766008f1bce2f796f4f13204ea6
maramagnus <mcieslik03@gmail.com>
2024-05-12 16:22:51 -0400

    saw a better way to code the __eq__ method for the TextNode class, changed the code to impliment

--- src/textnode.py
+++ src/textnode.py
@@ -1,17 +1,13 @@
+from typing import Self
+
 class TextNode:
     def __init__(self, text: str, text_type: str, url = None):
         self.text: str = text
         self.text_type: str = text_type
         self.url = url
 
-    def __eq__(self, other_node):
-        if (
-            self.text == other_node.text 
-            and self.text_type == other_node.text_type
-            and self.url == other_node.url
-        ):
-            return True
-        return False
-
+    def __eq__(self, other: Self):
+        return self.__dict__ == other.__dict__
+        
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
