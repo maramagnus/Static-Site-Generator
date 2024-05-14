55f0c0f92b114ea5752b1ac02d98c0103e79e7e9
maramagnus <mcieslik03@gmail.com>
2024-05-13 20:30:14 -0400

    small formatting changes

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -20,7 +20,7 @@ class HTMLNode:
         return f"HTMLNode:{self.tag, self.value, self.children, self.props}"
     
 class LeafNode(HTMLNode):
-    def __init__(self, tag: str, value: str, props: dict[str: str] = None):
+    def __init__(self, tag: str, value: str, props: dict[str: str] = None) -> None:
         super().__init__(tag, value, None, props)
 
     def to_html(self) -> str:
@@ -28,7 +28,6 @@ class LeafNode(HTMLNode):
             raise ValueError("All Leaf Nodes Require a Value")
         if self.tag == None:
             return self.value
-        
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
     
     def __repr__(self) -> str:
@@ -38,12 +37,12 @@ class ParentNode(HTMLNode):
     def __init__(self, tag: str, children: list[object], props: dict[str: str] = None) -> None:
         super().__init__(tag, None, children, props)
 
-    def to_html(self) -> None:
+    def to_html(self) -> str:
         if self.tag == "" or self.tag is None:
             raise ValueError("HTML error: No tag")
         if self.children == "" or self.children is None:
             raise ValueError("HTML error: No child nodes")
-        formatted_children = ""
+        formatted_children: str = ""
         for child in self.children:
             formatted_children += child.to_html()
         return f"<{self.tag}>{formatted_children}</{self.tag}>"
