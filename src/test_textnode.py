8ed1142072f75bc5ad599d8f501198b162d33936
maramagnus <mcieslik03@gmail.com>
2024-05-16 17:21:35 -0400

    more work on split_nodes_delimiter function

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,9 +9,20 @@ from textnode import split_nodes_delimiter
         node2 = TextNode("I like turtles", "bold")
         self.assertEqual(node1, node2)"""
 
-node1 = TextNode("I like **turtles**", "bold")
-node2 = "I am not a TextNode"
-print(split_nodes_delimiter([node1, node2], "**", "bold"))
+node1 = TextNode("I like **turtles** contains a bolded word", "text")
+node2 = TextNode("There are many cats, but **Mara** was special", "text")
+node3 = TextNode("*All of this text is italic*", "text")
+node4 = TextNode("the world is far *poorer* for the loss", "text")
+node5 = TextNode("This line contains `code snippets`", "text")
+node6 = TextNode("The solution to my problem turned out to be `this code block here`", "text")
+
+bold_nodes = [node1, node2]
+italic_nodes = [node3, node4]
+code_nodes = [node5, node6]
+
+print(split_nodes_delimiter(bold_nodes, "**", "bold"))
+print(split_nodes_delimiter(italic_nodes, "*", "italic"))
+print(split_nodes_delimiter(code_nodes, "`", "code"))
 
 
 """if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -28,20 +28,22 @@ def text_node_to_html_node(text_node: TextNode):
         return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
     raise Exception("Invalid Text Type")
 
-def split_nodes_delimiter(old_nodes: list, delimiter: str, text_type: str):
-    ret_nodes = []
+def split_nodes_delimiter(old_nodes: list, delimiter: str, text_type: str) -> list[TextNode]:
+    ret_nodes: list[TextNode] = []
     for node in old_nodes:
-        if type(node) != TextNode:
+        if node.text_type != "text":
             ret_nodes.append(node)
         else:    
             split_node: list[str] = (node.text).split(delimiter)
-        for i in range(0, len(split_node)):
-            if i % 2 == 0:
-                pass
-            if i % 2 != 0:
-                pass
-
-
-
-    
+            if len(split_node) % 2 == 0:
+                raise Exception("Invalid Markdown syntax: Missing a closing delimiter.")
+            
+            for i in range(0, len(split_node)):
+                if split_node[i] == "":
+                    continue
+                if i % 2 == 0:
+                    new_node = TextNode(split_node[i], "text")
+                if i % 2 != 0:
+                    new_node = TextNode(split_node[i], text_type)
+                ret_nodes.append(new_node)
     return ret_nodes
